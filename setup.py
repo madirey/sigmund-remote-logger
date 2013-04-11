d8ee4ba3d66fddb1d643cab11dc0e72ad921fe17
Matt Caldwell <matt.caldwell@gmail.com>
2013-04-11 06:15:45 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='peyote',
+      version='0.1',
+      description='Peyote is a remote logging service for Django.',
+      author='Matt Caldwell',
+      author_email='matt.caldwell@gmail.com',
+      url='https://github.com/mattcaldwell/peyote',
+      packages=['peyote']
+)
